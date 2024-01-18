25be6ef38061d5b43b30f20ca5b3379309f92b56
allen.dong <allen.dongzy@gmail.com>
2024-01-18 15:34:29 +0800

    feat: 添加login页重定向内容

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts" name="login"></script>
+<script setup lang="ts" name="login">
+import { queryToObj } from '@/utils/tools'
+
+// 路由参数
+const params = queryToObj(window.location.search)
+// 重定向地址
+const redirectPath = params.redirect || '/'
+
+// 登录
+const login = () => {
+  window.location.replace(redirectPath)
+}
+</script>
 
 <template>
-  <div class="login flex-row-center">login</div>
+  <div class="login flex-col-center">
+    login
+    <a-button @click="login">登录</a-button>
+  </div>
 </template>
 
 <style scoped lang="scss">
