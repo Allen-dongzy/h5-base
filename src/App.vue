c380deffdaf7dc5ec063f9a38ebc4ac63292b918
Allen Dong <Allen.Dongzy@gmail.com>
2025-03-21 18:01:37 +0800

    build(utils): 完善启动流程

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,16 @@ import 'dayjs/locale/zh-cn'
 import 'dayjs/locale/en'
 import dayjs from 'dayjs'
 import KeepAliveRouterView from '@/layout/KeepAliveRouterView.vue'
-import { debounce } from './utils/tools'
+import { debounce, queryToObj } from '@/utils/tools'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { useRouter } from 'vue-router'
+import useWecomStore from '@/stores/useWecomStore'
+import { register, getContext } from '@/utils/wecomUtils'
+import useUserStore from '@/stores/useUserStore'
+
+const userStore = useUserStore()
+const { token } = storeToRefs(userStore)
 
 // 设置路由进度条
 const router = useRouter()
@@ -54,6 +60,42 @@ const handleResize = debounce(function () {
 }, 300)
 window.addEventListener('resize', () => handleResize())
 handleResize()
+
+// 设置企微入口
+const { setEntry } = useWecomStore()
+// 初始化
+const init = async () => {
+  // 获取用户信息等
+  // js-sdk
+  const res = await register()
+  if (res) {
+    const res = await getContext()
+    if (!res) return
+    setEntry(res!.entry)
+  }
+}
+
+// 授权
+onMounted(async () => {
+  const pathNameArr = window.location.pathname.split('/')
+  // 白名单
+  if (['debug', 'login'].includes(pathNameArr[pathNameArr.length - 1])) return
+  // 已登录
+  if (token.value) return init()
+  // 本地开发走debug模式
+  if (import.meta.env.DEV) return router.replace('/debug')
+  // 跳转授权或code登录
+  const searshObj = queryToObj(window.location.search)
+  if (!searshObj?.code) {
+    // 获取OAuth2授权链接并跳转
+    console.log('获取OAuth2授权链接并跳转')
+  } else {
+    // OAuth2授权链接携带code重定向回来执行登录
+    console.log('OAuth2授权链接携带code重定向回来执行登录')
+    // 登录成功后执行初始化操作（jssdk，公共用户信息等）
+    // if (xxxxx) init()
+  }
+})
 </script>
 
 <template>
